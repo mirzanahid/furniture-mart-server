208a80ef65ebcb6fcf30f5890d23df1646b53361
mirzanahid <mirzasaikat3@gmail.com>
2022-11-25 15:11:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+//to use this template install express , mongodb , cors , dotenv
+
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000
+
+const app = express();
+
+//middle ware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vsmf7bv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+
+    try {
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(error => console.error(error))
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('server is running')
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
